73c0db401785bb8d309f67bc021adbeb66bc16ad
thasman6 <74593676+thasman6@users.noreply.github.com>
2022-03-15 18:31:43 -0700

    Update index.js
    fix / route

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('pages/index'))
+//app.get('/', (req, res) => res.render('pages/index'))
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
