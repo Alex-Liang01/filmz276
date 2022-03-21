2274b6643bda6c402c4475e540292de2c7b483b3
Alex-Liang01 <alexliang3@gmail.com>
2022-03-20 21:02:57 -0700

    comma missing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
