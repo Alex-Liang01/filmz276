2bbe2b8082d38905ab1a0386c4caa7f5629946ae
Kyle Santema <kbs2@sfu.ca>
2022-03-23 22:28:05 -0700

    mainpage updates

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL||'postgres://postgres:Oreo4831@localhost/proj',
+  //ssl: {
+  //  rejectUnauthorized: false
+  //}
 })
 
 const express = require('express')
@@ -41,13 +41,24 @@ app.get('/signup',(req,res)=>{
 })
 
 // ----------- MAIN PAGE -----------
-app.get('/',(req,res)=>{
+app.get('/',async(req,res)=>{
 	if (typeof req.session.user === 'undefined') {
 		res.redirect('loginn')
 	}
-	else {
-		res.render('pages/', {data: {user:val}})
+	try {
+
+    const base_url="https://api.themoviedb.org/3/movie/popular?"
+    const url=base_url+api_key+"&language=en-US&page=1"
+    const img_url="https://image.tmdb.org/t/p/w500/"
+    await fetch(url).then(res=>res.json()).then(data=>{
+      results=data.results.slice(0, 10);
+      res.render('pages/',{data: {user:val},results});
+    })
+
 	}
+  catch(err){
+    res.send(err);
+  }
   
 })
 
