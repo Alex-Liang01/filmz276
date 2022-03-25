868b1c66c90cabaf533e8ce8b24325849daa6005
Sanjit Mann <sumatmann@gmail.com>
2022-03-24 21:37:26 -0700

    visual imporvements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-   rejectUnauthorized: false
-  }
+  connectionString:  'postgres://postgres:sanjit12@localhost/users',
+  // ssl: {
+  //  rejectUnauthorized: false
+  // }
  
 })
 var cors = require("cors")
