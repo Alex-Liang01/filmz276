30216c4f57fa847de2282e4e8ddcc075dc1c63e6
Alex-Liang01 <alexliang3@gmail.com>
2022-04-04 13:07:24 -0700

    pushing to heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-  connectionString:'postgres://postgres:sanjit12@localhost/users',
-  // ssl: {
-  //  rejectUnauthorized: false
-  // }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+   rejectUnauthorized: false
+  }
 })
 var cors = require("cors") 
 
