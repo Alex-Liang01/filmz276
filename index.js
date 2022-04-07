516cd7cfa5dbff1bd549ac2e22c63823dd04f66f
Alex-Liang01 <alexliang3@gmail.com>
2022-04-06 17:31:04 -0700

    more tests for trending tv shows

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-  connectionString: 'postgres://postgres:123456789@localhost/proj'
-  // ssl: {
-  //  rejectUnauthorized: false
-  // }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+   rejectUnauthorized: false
+  }
 })
 var cors = require("cors") 
 
@@ -403,6 +403,16 @@ app.post('/logout', async(req,res) => {
       })
   });
 
+  app.get('/test_Tv_10', function(req, res) {
+    const base_url="https://api.themoviedb.org/3/tv/top_rated?"
+      const url=base_url+api_key+"&language=en-US&page=1"
+      const img_url="https://image.tmdb.org/t/p/w500/"
+      fetch(url).then(res=>res.json()).then(data=>{
+        results=data.results.slice(0, 10);
+        res.json(results);
+      })
+  });
+
   app.get('/trending',async(req,res)=>{
     if (typeof req.session.user === 'undefined') {
       res.redirect('loginn')
@@ -486,8 +496,16 @@ app.get('/tv_trending',async(req,res)=>{
 }
 })
 
-
-
+app.get('/testTrendingTv', function(req, res) {
+  const base_url="https://api.themoviedb.org/3/tv/popular?"
+  const url=base_url+api_key+"&language=en-US&page=1"
+  const img_url="https://image.tmdb.org/t/p/w154/"
+  fetch(url).then(res=>res.json()).then(data=>{
+    results=data.results.slice(0, 10);
+    res.json(results);
+  })
+  
+});
 
 
 // ----------- SUBMIT RATING -----------  
@@ -575,8 +593,10 @@ app.post('/submitrating', async(req,res) => {
       results=data
       res.json(results);
     })
-});
+  });
+
   us=[];
+  
   app.post('/testSignup', function(req, res) {
     signup_query=`....`
     //`INSERT INTO usr (username, password, firstname, lastname, email, birthday, gender) VALUES ('${username}','${password}','${firstname}','${lastname}','${email}','${birthday}','${gender}')`
@@ -636,8 +656,18 @@ app.post('/submitrating', async(req,res) => {
       })
   });
 
+  app.get('/test_TvIdFail', function(req, res) {
+    fetch("https://api.themoviedb.org/3/tv/10000000000000?api_key=430a4dbae6e33d3664541b0199ae6a38&language=en-US").then(res=>res.json()).then(data=>{
+      if(data.success==false){
+        res.json(data)
+        return;
+      }
+      results=data
+      res.json(results);
+    })
+});
 
-// ----------- MOVIE PAGE -----------
+  // ----------- MOVIE PAGE -----------
   app.get('/movie/:id',async(req,res)=>{
     if (typeof req.session.user === 'undefined') {
       res.redirect('/loginn')
@@ -693,6 +723,30 @@ app.post('/submitrating', async(req,res) => {
       res.send(err);
     }
   }
+  })
+
+  //Rendering main page for all other endpoints
+  app.get('*',async(req,res)=>{
+    if (typeof req.session.user === 'undefined') {
+      res.redirect('loginn')
+    }
+    else{
+    try {
+  
+      const base_url="https://api.themoviedb.org/3/movie/popular?"
+      const url=base_url+api_key+"&language=en-US&page=1"
+      const img_url="https://image.tmdb.org/t/p/w500/"
+      await fetch(url).then(res=>res.json()).then(data=>{
+        results=data.results.slice(0, 10);
+        res.render('pages/',{data: {user:val},results});
+      })
+  
+    }
+    catch(err){
+      res.send(err);
+    }
+  }
+    
   })
 
   module.exports = app;
